fa2c4626ea6cf42ab505c9892a01e354a40332dd
Will Marttala <wmarttala123@gmail.com>
2023-07-20 13:39:01 -0500

    Added video_cam handler to demonstrate process messaging functionality

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,6 +7,7 @@ defmodule Servy.Handler do
 
   alias Servy.Conv
   alias Servy.BearController
+  alias Servy.VideoCam
 
   @pages_path Path.expand("pages", File.cwd!())
 
@@ -91,9 +92,26 @@ defmodule Servy.Handler do
     |> handle_file(conv)
   end
 
+  def route(%Conv{method: "GET", path: "/snapshots"} = conv) do
+    parent = self() # the request-handling process
+
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-1")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-2")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-3")}) end)
+
+    snapshot1 = receive do {:result, filename} -> filename end
+    snapshot2 = receive do {:result, filename} -> filename end
+    snapshot3 = receive do {:result, filename} -> filename end
+
+    snapshots = [snapshot1, snapshot2, snapshot3]
+
+    %{ conv | status: 200, resp_body: inspect snapshots}
+  end
+
   def route(%Conv{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
   end
+
 
   def put_content_length(conv) do
     headers = Map.put(conv.resp_headers, "Content-Length", byte_size(conv.resp_body))
